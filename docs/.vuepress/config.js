1c9ccc5a65d144072806a32a569ff499a68c377d
lwj1426282929 <5366586+lwj1426282929@user.noreply.gitee.com>
2020-02-25 13:19:08 +0800

    优化动态目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-const Sidebar = require('./config.sidebar');
+const Menu = require('./config.menu');
 
 module.exports = {
   // 端口， 默认8080
@@ -21,7 +21,7 @@ module.exports = {
 
   // 主题配置
   themeConfig: {
-    sidebarDepth: 2, // 显示 h1, h2, h3
+    sidebarDepth: 1, // 显示 h1, h2
     repo: 'https://github.com/lwj1426282929/vue-press-blog.git',
     locales: {
       '/': {
@@ -32,33 +32,12 @@ module.exports = {
         nav: [
           { text: '首页', link: '/' },
           { text: '笔记', link: '/docs/' },
-          {
-            text: '技术笔记',
-            items: [
-              { text: 'HTML', link: '/docs/HTML/' },
-              { text: 'CSS', link: '/docs/CSS/' },
-              { text: 'JavaScript', link: '/docs/JavaScript/' },
-              { text: 'Vue', link: '/docs/Vue/' },
-              { text: 'React', link: '/docs/React/' },
-              { text: 'Angular', link: '/docs/Angular/' },
-              { text: 'Node', link: '/docs/Node/' },
-              { text: '其他', link: '/docs/Others/' },
-            ],
-          },
+          { text: '技术笔记', items: Menu.getNavs() },
           { text: '关于我', link: 'http://144.34.173.6/' },
         ],
         sidebar: {
-          '/docs/HTML/': Sidebar.getMenus('HTML'),
-          '/docs/CSS/': Sidebar.getMenus('CSS'),
-          '/docs/JavaScript/': Sidebar.getMenus('JavaScript'),
-          '/docs/Vue/': Sidebar.getMenus('Vue'),
-          '/docs/React/': Sidebar.getMenus('React'),
-          '/docs/Angular/': Sidebar.getMenus('Angular'),
-          '/docs/Node/': Sidebar.getMenus('Node'),
-          '/docs/Others/': Sidebar.getMenus('其他'),
-          '/docs/EnglishEssay/': Sidebar.getMenus('English Essay'),
-          '/docs/': Sidebar.getMenus(),
-        },
+          '/docs/': Menu.getContents()
+        }
       },
     },
   },
---
+++ docs/.vuepress/config.menu.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+class Menu {
+  // 动态生成目录树(含完整路径)
+  getContents(path_ = '../docs') {
+    const findFilesByPath = (path_, content = []) => {
+      let files = fs.readdirSync(path.join(__dirname, path_));
+      files.forEach(file => {
+        let subPath = path_ + '/' + file;
+        let stat = fs.lstatSync(path.join(__dirname, subPath));
+        let isDirectory = stat.isDirectory();
+        if (isDirectory) {
+          let children = findFilesByPath(subPath);
+          content.push({
+            title: file,
+            children,
+            _path: subPath.replace('../docs/', ''),
+          });
+        } else if (file !== 'README.md') {
+          content.push(subPath.replace('../docs/', ''));
+        }
+      });
+      return content;
+    };
+
+    return findFilesByPath(path_);
+  }
+
+  // 动态生成Nav
+  getNavs() {
+    let contents = this.getContents();
+    let navs = [];
+    contents.forEach(content => {
+      navs.push({ text: content.title, link: content._path + '/' });
+    });
+    return navs;
+  }
+}
+
+module.exports = new Menu();
