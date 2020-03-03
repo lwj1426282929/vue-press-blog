fae48f1151ff06bbaf72d4bb5a9931822199bc13
lwj1426282929 <5366586+lwj1426282929@user.noreply.gitee.com>
2020-03-03 12:54:47 +0800

    添加代码复制功能

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,8 +1,6 @@
 import Element from 'element-ui';
 import './styles/element-#3EAF7C/index.css';
 
-import copy from './common/copy';
-
 export default ({ Vue }) => {
   Vue.use(Element);
   setTimeout(() => {
