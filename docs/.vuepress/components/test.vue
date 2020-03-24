211c25ee551aaa52ea8e3fc3d0ac78836bd52107
lwj1426282929 <5366586+lwj1426282929@user.noreply.gitee.com>
2020-03-24 14:44:37 +0800

    添加队列和栈

---
+++ docs/.vuepress/components/test.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>123</div>
+</template>
+
+<script>
+class Stack {
+    #dataStore
+
+    constructor (arr = []) {
+        this.#dataStore = arr;
+    }
+
+    // 入栈
+    push (item) {
+        this.#dataStore.push(item);
+    }
+
+    // 出栈
+    pop () {
+        this.#dataStore.pop();
+    }
+
+    // 栈顶元素
+    peek () {
+        return this.#dataStore[this.#dataStore.length - 1]
+    }
+
+    // 清空栈
+    clear () {
+        this.#dataStore = []
+    }
+
+    // 站内元素
+    value () {
+        return [...this.#dataStore];
+    }
+
+    // 栈长度
+    length () {
+        return this.#dataStore.length
+    }
+
+    // 判断是否为空
+    isEmpty () {
+        return !this.#dataStore.length
+    }
+}
+export default {
+    name: 'test',
+
+    created () {
+        let Sta = new Stack([1, 2, 3, 4, 5, 6])
+        Sta.push(7)
+        console.log(Sta.value());
+        Sta.pop()
+        console.log(Sta.value());
+        console.log(Sta.peek());
+    }
+}
+</script>
