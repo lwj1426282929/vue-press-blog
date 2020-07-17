345e26297db223023d630a7dc0f2bb9f11918d86
lwj1426282929 <1426282929@qq.com>
2020-07-17 15:55:31 +0800

    修改样式

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,5 @@
 import Element from 'element-ui';
 import './styles/element-#3EAF7C/index.css';
-import 'highlight.js/styles/color-brewer.css';
 import Packages from './packages';
 
 export default ({ Vue, options, router }) => {
