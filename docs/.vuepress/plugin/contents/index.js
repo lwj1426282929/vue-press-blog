6a8c2180dcf2a6df2460286bed3b97934d81f611
lwj1426282929 <1426282929@qq.com>
2020-07-20 15:13:17 +0800

    添加HTML5新元素文档

--- docs/.vuepress/plugin/contents/index.js
+++ docs/.vuepress/plugin/contents/index.js
@@ -6,8 +6,8 @@ module.exports = (options, ctx) => {
         extendPageData($page) {
             const path_ = $page.path.match(/^\/(\S*)\/$/);
             if (path_) {
-                let pathStr = path_[0].substring(0, path_[0].length - 1)
-                let contents = Menu.getContents('../../../docs' + decodeURI(pathStr));
+                const pathStr = path_[0].substring(0, path_[0].length - 1)
+                const contents = Menu.getContents('../../../docs' + decodeURI(pathStr));
                 $page.contents = contents;
             }
         }
